f38fdafa307a5da70c59c63d587bce637afb7b30
Scott Brady <scott@prolio.com>
2017-09-06 09:47:53 -0400

    Homework2

---
+++ Homework 2/HelloWorld.java
@@ -0,0 +1,15 @@
+/******************************************************************************
+ CLASS INFORMATION                                    * Score: ______________ *
+ -----------------                                    *************************
+   Programmer: Sam Aponte
+   Date Due:  9/6/2017
+ ******************************************************************************/
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		// Print out a message to the whole world!
+		System.out.println("I changed the helloworld to this !");
+	}
+}
