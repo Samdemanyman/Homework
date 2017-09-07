8888337bd32c379dc3f890f6e605232a33dddace
Scott Brady <scott@prolio.com>
2017-09-07 10:29:24 -0400

    changed 4 to 2

--- Homework 4/HelloWorld.java
+++ Homework 4/HelloWorld.java
@@ -10,6 +10,13 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		// Print out a message to the whole world!
-		System.out.println("I changed the helloworld to this !");
+	String start;
+	
+	start = (" read me ");
+	
+	
+	
+		System.out.println(start);
 	}
 }
+  
