56335c70a7f4d1e88123fc79ab6c9f7bcba905b8
Scott Brady <scott@prolio.com>
2017-09-15 09:28:40 -0400

    Homework 5
    Homework 5

---
+++ Homework 5/HelloWorld.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+/******************************************************************************
+ CLASS INFORMATION                                    * Score: ______________ *
+ -----------------                                    *************************
+   Programmer: Sam Aponte
+   Date Due:  9/6/2017
+ ******************************************************************************/
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+	    Scanner Samuel;
+		Samuel = new Scanner(System.in);
+	   	
+		
+		String b;
+		String a;
+		String start;
+		String stop;
+		start = ("Hey, Whats your name?");
+		stop = ("Nice to meet you"); 
+		System.out.println(start);
+		b=Samuel.nextLine();
+		System.out.println(stop + " " + b );
+		 a= Samuel.nextLine();
+		
+		
+		
+	   
+		
+		
+
+
+	}
+}
+  
